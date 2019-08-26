d8701353fa73b60922ff0cd5792059faa40bf2ec
shankars <shankar.shinde@nitorinfotech.com>
2019-08-26 09:33:22 +0530

    First commit

---
+++ VaryForTraitesDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VaryForTraitesDemo
+//
+//  Created by shankars on 8/9/19.
+//  Copyright © 2019 shankars. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
